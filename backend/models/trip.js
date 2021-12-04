a47c6cedb65280f2a4b5e4334d87b84a34fbde7e
krishnagupta71 <gupta.krishna71@gmail.com>
2021-12-03 19:44:32 -0800

    changed getTripsOfUser api to show trip even if bill is not present

--- backend/models/trip.js
+++ backend/models/trip.js
@@ -1,58 +1,57 @@
-import dbConn from'../config/db.config.js';
+import dbConn from "../config/db.config.js";
 
-var TripModel = function(trip){
-    this.tripID = trip.tripID,
-    this.dropoff_location = trip.dropoff_location,
-    this.start_time = trip.start_time,
-    this.end_time = trip.end_time,
-    this.pickup_location = trip.pickup_location,
-    this.iscompleted = trip.iscompleted,
-    this.userID = trip.userID,
-    this.carID = trip.carID,
-    this.atPickUp = trip.atPickUp,
-    this.collision = trip.collision,
-    this.eta = trip.eta
-}
+var TripModel = function (trip) {
+  (this.tripID = trip.tripID),
+    (this.dropoff_location = trip.dropoff_location),
+    (this.start_time = trip.start_time),
+    (this.end_time = trip.end_time),
+    (this.pickup_location = trip.pickup_location),
+    (this.iscompleted = trip.iscompleted),
+    (this.userID = trip.userID),
+    (this.carID = trip.carID),
+    (this.atPickUp = trip.atPickUp),
+    (this.collision = trip.collision),
+    (this.eta = trip.eta);
+};
 
 //get all tripss
-TripModel.getAllTrips = (result)=>{
-    dbConn.query('SELECT * FROM trip',(err, res)=>{
-        if(err){
-            console.log('Error while fetching trips', err)
-            result(err, null)
-        }
-        else{
-            result(null, res);
-        }
-    })
-}
+TripModel.getAllTrips = (result) => {
+  dbConn.query("SELECT * FROM trip", (err, res) => {
+    if (err) {
+      console.log("Error while fetching trips", err);
+      result(err, null);
+    } else {
+      result(null, res);
+    }
+  });
+};
 
 //get tripByID from DB
-TripModel.getTripByID = (id, result)=>{
-    dbConn.query(`SELECT * FROM trip WHERE tripID=?`, id ,  (err, res)=>{
-        if(err){
-            console.log('Error while fetching trips', err)
-            result(err, null)
-        }
-        else{
-            result(null, res);
-        }
-    })
-}
+TripModel.getTripByID = (id, result) => {
+  dbConn.query(`SELECT * FROM trip WHERE tripID=?`, id, (err, res) => {
+    if (err) {
+      console.log("Error while fetching trips", err);
+      result(err, null);
+    } else {
+      result(null, res);
+    }
+  });
+};
 
-TripModel.getIdlecar = (id, result)=>{    
-   // dbConn.query(`SELECT carID FROM trip WHERE iscompleted = 1` ,  (err, res)=>{
-    dbConn.query(`SELECT carID FROM car WHERE carID NOT IN (SELECT carID FROM trip WHERE iscompleted = 1)` ,  (err, res)=>{
-        if(err){
-            console.log('Error while fetching trips', err)
-            result(err, null)
-        }
-        else{
-            result(null, res);
-        }
-    })
-}
-
+TripModel.getIdlecar = (id, result) => {
+  // dbConn.query(`SELECT carID FROM trip WHERE iscompleted = 1` ,  (err, res)=>{
+  dbConn.query(
+    `SELECT carID FROM car WHERE carID NOT IN (SELECT carID FROM trip WHERE iscompleted = 1)`,
+    (err, res) => {
+      if (err) {
+        console.log("Error while fetching trips", err);
+        result(err, null);
+      } else {
+        result(null, res);
+      }
+    }
+  );
+};
 
 /* POST JSON Details for Insomnia to create a new trip
 {
@@ -67,31 +66,44 @@ TripModel.getIdlecar = (id, result)=>{
     "carID": 1
 }*/
 
-
 TripModel.createTrip = (tripReqData, result) => {
-    dbConn.query('INSERT INTO trip SET ?', tripReqData ,  (err, res)=>{
-        if(err){
-            console.log('Error while inserting trip data', err)
-            result(err, null)
-        }
-        else{
-            result(null, res);
-        }
-    })
-}
+  dbConn.query("INSERT INTO trip SET ?", tripReqData, (err, res) => {
+    if (err) {
+      console.log("Error while inserting trip data", err);
+      result(err, null);
+    } else {
+      result(null, res);
+    }
+  });
+};
 
 //Update Trip
 TripModel.updateTrip = (id, tripReqData, result) => {
-   dbConn.query('UPDATE trip SET dropoff_location=?, start_time = ?, end_time = ?, pickup_location = ?, userID = ?, iscompleted = ?, carID = ?, atPickUp = ?, collision = ?, eta = ? WHERE tripID = ?',[tripReqData.dropoff_location, tripReqData.start_time, tripReqData.end_time, tripReqData.pickup_location, tripReqData.userID, tripReqData.iscompleted, tripReqData.carID, tripReqData.atPickUp, tripReqData.collision, tripReqData.eta, id], (err, res)=>{
-   if(err){
-            console.log('Error while updating trip data', err)
-            result(err, null)
-        }
-        else{
-            result(null, res);
-        }
-    });
-}
+  dbConn.query(
+    "UPDATE trip SET dropoff_location=?, start_time = ?, end_time = ?, pickup_location = ?, userID = ?, iscompleted = ?, carID = ?, atPickUp = ?, collision = ?, eta = ? WHERE tripID = ?",
+    [
+      tripReqData.dropoff_location,
+      tripReqData.start_time,
+      tripReqData.end_time,
+      tripReqData.pickup_location,
+      tripReqData.userID,
+      tripReqData.iscompleted,
+      tripReqData.carID,
+      tripReqData.atPickUp,
+      tripReqData.collision,
+      tripReqData.eta,
+      id,
+    ],
+    (err, res) => {
+      if (err) {
+        console.log("Error while updating trip data", err);
+        result(err, null);
+      } else {
+        result(null, res);
+      }
+    }
+  );
+};
 
 // //Update Pickup
 // TripModel.updatePickup = (id, tripReqData, result) => {
@@ -106,7 +118,7 @@ TripModel.updateTrip = (id, tripReqData, result) => {
 //      });
 //  }
 
-//Update Trip Finished 
+//Update Trip Finished
 // TripModel.updateFinishedTrip = (tripReqData, result) => {
 //     dbConn.query('UPDATE trip SET end_time = NOW(), iscompleted = 1 WHERE tripID = ?',[tripReqData.tripID], (err, res)=>{
 //     if(err){
@@ -121,42 +133,46 @@ TripModel.updateTrip = (id, tripReqData, result) => {
 
 //Update At PickUP
 TripModel.updateAtPickUP = (tripReqData, result) => {
-    dbConn.query('UPDATE trip SET atPickUP = 1 WHERE tripID = ?',[tripReqData.tripID], (err, res)=>{
-    if(err){
-             console.log('Error while updating trip data', err)
-             result(err, null)
-         }
-         else{
-             result(null, res);
-         }
-     });
- }
+  dbConn.query(
+    "UPDATE trip SET atPickUP = 1 WHERE tripID = ?",
+    [tripReqData.tripID],
+    (err, res) => {
+      if (err) {
+        console.log("Error while updating trip data", err);
+        result(err, null);
+      } else {
+        result(null, res);
+      }
+    }
+  );
+};
 
 //Delete Trip
 TripModel.deleteTrip = (id, result) => {
-    dbConn.query('DELETE FROM trip WHERE tripID = ?', id, (err, res)=>{
-        if(err){
-             console.log('Error while deleting trip data', err)
-             result(err, null)
-         }
-         else{
-             result(null, res);
-         }
-     });
- }
+  dbConn.query("DELETE FROM trip WHERE tripID = ?", id, (err, res) => {
+    if (err) {
+      console.log("Error while deleting trip data", err);
+      result(err, null);
+    } else {
+      result(null, res);
+    }
+  });
+};
 
+//get tripByID from DB
+TripModel.getTripByUser = (id, result) => {
+  dbConn.query(
+    `SELECT * FROM trip left join billing on trip.tripID = billing.tripID  where trip.userID = ?`,
+    id,
+    (err, res) => {
+      if (err) {
+        console.log("Error while fetching trips", err);
+        result(err, null);
+      } else {
+        result(null, res);
+      }
+    }
+  );
+};
 
- //get tripByID from DB
-TripModel.getTripByUser = (id, result)=>{
-    dbConn.query(`SELECT * FROM trip join billing on trip.tripID = billing.tripID  where trip.userID = ?`, id ,  (err, res)=>{
-        if(err){
-            console.log('Error while fetching trips', err)
-            result(err, null)
-        }
-        else{
-            result(null, res);
-        }
-    })
-}
-
-export default TripModel ;
+export default TripModel;
