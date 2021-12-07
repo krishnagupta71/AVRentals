f4cb9843497c4772d88d25dfebe32ea3cf8c9616
krishnagupta71 <gupta.krishna71@gmail.com>
2021-12-07 09:11:16 -0800

    ec2 port changes

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,29 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose' ;
-import cors from 'cors' ;
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
 //import userRoutes
-import userRoutes from './routes/user.js' ;
-import tripRoutes from './routes/trip.js' ;
-import carRoutes from './routes/car.js' ;
-import billingRoutes from './routes/billing.js' ;
+import userRoutes from "./routes/user.js";
+import tripRoutes from "./routes/trip.js";
+import carRoutes from "./routes/car.js";
+import billingRoutes from "./routes/billing.js";
 // import idlecarRoutes from './routes/idlecar.js' ;
-const app = express() ;
+const app = express();
 
-app.use(express.json({limit:"20mb", extended : true}));
-app.use(express.urlencoded({limit:"20mb", extended : true}));
+app.use(express.json({ limit: "20mb", extended: true }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
-app.use(cors()) ;
+app.use(cors());
 
 //create routes
-app.use('/', userRoutes);
-app.use('/trip', tripRoutes);
-app.use('/car', carRoutes);
-app.use('/billing', billingRoutes);
+app.use("/", userRoutes);
+app.use("/trip", tripRoutes);
+app.use("/car", carRoutes);
+app.use("/billing", billingRoutes);
 // app.use('/idlecar', idlecarRoutes);
 
-const PORT = 3000 ;
-app.listen(PORT,()=>{
-    console.log(`Express Server is running at port: ${PORT}`)
-})
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Express Server is running at port: ${PORT}`);
+});
--- frontend/src/app/Api.js
+++ frontend/src/app/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL = "http://18.188.8.34:3000";
+const API_BASE_URL = "http://18.188.8.34:4000";
 // const API_BASE_URL = "http://localhost:3001";
 
 export async function post({ endpoint, body }) {
